de955592f80f4b875ca859c04089e7f9de9ee9e3
Owen0603 <ldmac@ldmacdeimac.local>
2016-03-07 10:55:19 +0800

    Initial Commit

---
+++ HttpsDemo_OC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HttpsDemo_OC
+//
+//  Created by  ldmac on 16/3/7.
+//  Copyright © 2016年  ldmac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HttpsDemo_OC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HttpsDemo_OC
+//
+//  Created by  ldmac on 16/3/7.
+//  Copyright © 2016年  ldmac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
